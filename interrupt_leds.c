40edb4f9cd5538e9a70c889975d634a075a8de40
Isabella <isa021andrade@gmail.com>
2025-01-31 09:41:06 -0300

    feat: create main loop
    Red LED keeps blinking in a infinite loop

--- interrupt_leds.c
+++ interrupt_leds.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+// Definição dos pinos
+const uint pin_led_red = 13;
+
 int main()
 {
     stdio_init_all();
-
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+    // Inicialização e configuração do LED
+    gpio_init(pin_led_red);
+    gpio_set_dir(pin_led_red, GPIO_OUT);
+ 
+    // Loop principal, LED vermelho pisca constantemente
+    while (true){
+        gpio_put(pin_led_red, false);
+        sleep_ms(100);
+        gpio_put(pin_led_red, true);
+        sleep_ms(100);
     }
 }
