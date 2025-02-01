d2450d74bae00b1623cc0b415607205c14fbf974
Isabella <isa021andrade@gmail.com>
2025-02-01 11:08:20 -0300

    feat: add number patterns for LED matrix

--- interrupt_leds.c
+++ interrupt_leds.c
@@ -6,50 +6,139 @@
 #include "interrupt_leds.pio.h"
 
 // Definição dos pinos
-const uint pin_led_red = 13;
-const uint pin_led_blue = 12;
-const uint btn_A = 5;
-const uint btn_B = 6;
+const uint PIN_LED_RED = 13;
+const uint PIN_LED_BLUE = 12;
+const uint BTN_A = 5;
+const uint BTN_B = 6;
 
 #define NUM_PIXELS 25
 #define OUT_PIN 7
+volatile int current_pattern = 0;
 
-double numeros[2][5][5] = {
-    {// Número 1
-     {0.0, 0.3, 0.3, 0.3, 0.0},
-     {0.0, 0.3, 0.0, 0.3, 0.0},
-     {0.0, 0.3, 0.3, 0.3, 0.0},
-     {0.0, 0.3, 0.0, 0.3, 0.0},
-     {0.0, 0.3, 0.3, 0.3, 0.0}},
-    {// Número 2
-     {1.0, 0.0, 0.0, 0.0, 1.0},
-     {0.0, 1.0, 0.0, 1.0, 0.0},
-     {0.0, 0.0, 1.0, 0.0, 0.0},
-     {0.0, 1.0, 0.0, 1.0, 0.0},
-     {1.0, 0.0, 0.0, 0.0, 1.0}}};
+double numeros[11][5][5] = {
+    {
+        // Apagar
+        {0.0, 0.0, 0.0, 0.0, 0.0},
+        {0.0, 0.0, 0.0, 0.0, 0.0},
+        {0.0, 0.0, 0.0, 0.0, 0.0},
+        {0.0, 0.0, 0.0, 0.0, 0.0},
+        {0.0, 0.0, 0.0, 0.0, 0.0},
+    },
+    {
+        // Número 0
+        {0.0, 0.1, 0.1, 0.1, 0.0},
+        {0.0, 0.1, 0.0, 0.1, 0.0},
+        {0.0, 0.1, 0.0, 0.1, 0.0},
+        {0.0, 0.1, 0.0, 0.1, 0.0},
+        {0.0, 0.1, 0.1, 0.1, 0.0},
+    },
+    {
+        // Número 1
+        {0.0, 0.1, 0.1, 0.1, 0.0},
+        {0.0, 0.0, 0.1, 0.0, 0.0},
+        {0.0, 0.0, 0.1, 0.0, 0.0},
+        {0.0, 0.1, 0.1, 0.0, 0.0},
+        {0.0, 0.0, 0.1, 0.0, 0.0},
+    },
+    {
+        // Número 2
+        {0.0, 0.1, 0.1, 0.1, 0.0},
+        {0.0, 0.1, 0.0, 0.0, 0.0},
+        {0.0, 0.1, 0.1, 0.1, 0.0},
+        {0.0, 0.0, 0.0, 0.1, 0.0},
+        {0.0, 0.1, 0.1, 0.1, 0.0},
+    },
+    {
+        // Número 3
+        {0.0, 0.1, 0.1, 0.1, 0.0},
+        {0.0, 0.0, 0.0, 0.1, 0.0},
+        {0.0, 0.1, 0.1, 0.0, 0.0},
+        {0.0, 0.0, 0.0, 0.1, 0.0},
+        {0.0, 0.1, 0.1, 0.1, 0.0},
+    },
+    {
+        // Número 4
+        {0.0, 0.1, 0.0, 0.0, 0.0},
+        {0.0, 0.0, 0.0, 0.1, 0.0},
+        {0.0, 0.1, 0.1, 0.1, 0.0},
+        {0.0, 0.1, 0.0, 0.1, 0.0},
+        {0.0, 0.1, 0.0, 0.1, 0.0},
+    },
+    {
+        // Número 5
+        {0.0, 0.1, 0.1, 0.1, 0.0},
+        {0.0, 0.0, 0.0, 0.1, 0.0},
+        {0.0, 0.1, 0.1, 0.1, 0.0},
+        {0.0, 0.1, 0.0, 0.0, 0.0},
+        {0.0, 0.1, 0.1, 0.1, 0.0},
+    },
+    {
+        // Número 6
+        {0.0, 0.1, 0.1, 0.1, 0.0},
+        {0.0, 0.1, 0.0, 0.1, 0.0},
+        {0.0, 0.1, 0.1, 0.1, 0.0},
+        {0.0, 0.1, 0.0, 0.0, 0.0},
+        {0.0, 0.1, 0.1, 0.1, 0.0},
+    },
+    {
+        // Número 7
+        {0.0, 0.1, 0.0, 0.0, 0.0},
+        {0.0, 0.0, 0.0, 0.1, 0.0},
+        {0.0, 0.1, 0.0, 0.0, 0.0},
+        {0.0, 0.0, 0.0, 0.1, 0.0},
+        {0.0, 0.1, 0.1, 0.1, 0.0},
+    },
+    {
+        // Número 8
+        {0.0, 0.1, 0.1, 0.1, 0.0},
+        {0.0, 0.1, 0.0, 0.1, 0.0},
+        {0.0, 0.1, 0.1, 0.1, 0.0},
+        {0.0, 0.1, 0.0, 0.1, 0.0},
+        {0.0, 0.1, 0.1, 0.1, 0.0},
+    },
+    {
+        // Número 8
+        {0.0, 0.1, 0.1, 0.1, 0.0},
+        {0.0, 0.0, 0.0, 0.1, 0.0},
+        {0.0, 0.1, 0.1, 0.1, 0.0},
+        {0.0, 0.1, 0.0, 0.1, 0.0},
+        {0.0, 0.1, 0.1, 0.1, 0.0},
+    },
+};
 
 uint32_t matrix_rgb(double r, double g, double b)
 {
     return ((uint8_t)(g * 255) << 24) | ((uint8_t)(r * 255) << 16) | ((uint8_t)(b * 255) << 8);
 }
 
-void acender_todos_leds(PIO pio, uint sm, double r, double g, double b)
+void acender_todos_leds(PIO pio, uint sm, int current_pattern)
 {
-
-    for (int i = 0; i < 5; i++)
+    for (int j = 0; j < 5; j++)
     {
-        for (int j = 0; j < 5; j++)
+        for (int k = 0; k < 5; k++)
         {
-            uint32_t cor = matrix_rgb(numeros[0][i][j], g, b);
+            uint32_t cor = matrix_rgb(numeros[current_pattern][j][k], 0, 0);
             pio_sm_put_blocking(pio, sm, cor);
         }
     }
+    sleep_ms(100);
 }
 
 void gpio_irq_handler(uint gpio, uint32_t events)
 {
-    bool estado_atual = gpio_get(pin_led_blue);
-    gpio_put(pin_led_blue, !estado_atual);
+    if (gpio_get(BTN_A))
+    {
+        bool estado_atual = gpio_get(PIN_LED_BLUE);
+        gpio_put(PIN_LED_BLUE, !estado_atual);
+        current_pattern--;
+    }
+
+    else
+    {
+        bool estado_atual = gpio_get(PIN_LED_BLUE);
+        gpio_put(PIN_LED_BLUE, !estado_atual);
+        current_pattern++;
+    }
 }
 
 int main()
@@ -64,30 +153,30 @@ int main()
 
     stdio_init_all();
     // Inicialização e configuração do LED
-    gpio_init(pin_led_red);
-    gpio_set_dir(pin_led_red, GPIO_OUT);
-    gpio_init(pin_led_blue);
-    gpio_set_dir(pin_led_blue, GPIO_OUT);
+    gpio_init(PIN_LED_RED);
+    gpio_set_dir(PIN_LED_RED, GPIO_OUT);
+    gpio_init(PIN_LED_BLUE);
+    gpio_set_dir(PIN_LED_BLUE, GPIO_OUT);
 
     // Inicialização e configuração dos botões
-    gpio_init(btn_A);
-    gpio_set_dir(btn_A, GPIO_IN);
-    gpio_pull_up(btn_A);
-    gpio_init(btn_B);
-    gpio_set_dir(btn_B, GPIO_IN);
-    gpio_pull_up(btn_B);
+    gpio_init(BTN_A);
+    gpio_set_dir(BTN_A, GPIO_IN);
+    gpio_pull_up(BTN_A);
+    gpio_init(BTN_B);
+    gpio_set_dir(BTN_B, GPIO_IN);
+    gpio_pull_up(BTN_B);
 
-    gpio_set_irq_enabled_with_callback(btn_A, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
-    gpio_set_irq_enabled_with_callback(btn_B, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+    gpio_set_irq_enabled_with_callback(BTN_A, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+    gpio_set_irq_enabled_with_callback(BTN_B, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
 
     // Loop principal, LED vermelho pisca constantemente
     while (true)
     {
-        acender_todos_leds(pio, sm, 0, 0, 0);
-        sleep_ms(1000);
-        gpio_put(pin_led_red, false);
+        acender_todos_leds(pio, sm, current_pattern);
+
+        gpio_put(PIN_LED_RED, false);
         sleep_ms(100);
-        gpio_put(pin_led_red, true);
+        //gpio_put(PIN_LED_RED, true);
         sleep_ms(100);
     }
 }
